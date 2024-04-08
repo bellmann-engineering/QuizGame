5ebace699a8759e3d30dc8c60c3253da43ed56ee
Kai <kb@bellmann-engineering.com>
2024-04-08 15:25:36 +0200

    changed text to english, closes #1

--- QuizGame/Program.vb
+++ QuizGame/Program.vb
@@ -2,10 +2,10 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hallo Welt!")
+        Console.WriteLine("Hello Wórld!")
 
         For i = 0 To 10 Step 1
-            Console.WriteLine("schleifenlauf")
+            Console.WriteLine("loop iteration")
         Next i
 
     End Sub
