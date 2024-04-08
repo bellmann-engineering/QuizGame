2dc1599643cf5de6a51617c3daf2c9111f38db09
Kai <kb@bellmann-engineering.com>
2024-04-08 11:02:43 +0200

    added for loop

--- QuizGame/Program.vb
+++ QuizGame/Program.vb
@@ -3,5 +3,10 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hallo Welt!")
+
+        For i = 0 To 5 Step 1
+            Console.WriteLine("Test")
+        Next i
+
     End Sub
 End Module
